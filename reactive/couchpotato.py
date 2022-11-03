8b6aad32372b095abd9806af6df9f3f330cdc034
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 08:58:55 +0000

    Adding tarfile member sanitization to extractall()

--- reactive/couchpotato.py
+++ reactive/couchpotato.py
@@ -52,7 +52,26 @@ def setup_config():
         backup_file = hookenv.resource_get('couchconfig')
         if backup_file:
             with tarfile.open(backup_file, 'r:gz') as inFile:
-                inFile.extractall(cp.config_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(inFile, cp.config_dir)
             host.chownr(cp.home_dir, owner=cp.user, group=cp.user)
             cp.reload_config()
             cp.set_indexers(False)
